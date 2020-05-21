26dbd3256a20c01732712f74f0cab7624efb17b3
Youngung Jeong <yjeong@changwon.ac.kr>
2020-05-21 14:06:17 +0900

    variable velocity added

---
+++ vel_var/var_vel_cheat_sheet.f
@@ -0,0 +1,22 @@
+      program var_vel
+      implicit none
+      character*12 cdt
+      integer i
+      real eps, t, l, dt
+
+c     initial condition
+      l=5.
+      t=0.
+      eps=0.
+      dt=0.01
+
+      open(3,file='var_vel.txt')
+      do while(t.lt.30)
+         write(3,'(3f10.5)')  t,l,eps
+         eps=eps+cos(t)/l * dt
+         l = l + cos(t)*dt
+         t = t + dt
+      enddo
+      close(3)
+
+      end program
