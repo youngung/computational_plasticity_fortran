63708c7676e89103d02f3bd04aa6d0e5ea3d8ec6
Youngung Jeong <yjeong@changwon.ac.kr>
2020-05-23 21:18:02 +0900

    Updates before trying something else

--- elasto_plastic_scalar/elasto_plasticity_scalar.f
+++ elasto_plastic_scalar/elasto_plasticity_scalar.f
@@ -1,10 +1,70 @@
+c     elasto-plasticity without strain-hardening
+
 c     scalar yield function f(s) = sqrt(s**2)
+c     Homogeneous function of degree 1
       real function calc_yield_function(stress)
       implicit none
       real stress
       calc_yield_function = sqrt(stress**2.)
       return
       end function
+c     --------------------------------------------
+      program elasto_plasticity_scalar
+      implicit none
+      real calc_yield_function
+      real dt, E, c, t, l, dl, eps, deps, deps_el, deps_pl,dsig,
+     $     stress
+      real vel, f, tol
+      integer kount,iplast
+      parameter(tol=1e-6)
+
+      open(3,file='elasto_plasticity_scalar.txt')
+      dt = 0.023                  ! time increment
+      E = 200000                ! elastic modulus
+      c = 200.                   ! yield criterion
+      stress = 0.               ! initial stress
+      eps = 0.                  ! initial strain
+      l = 1.                    ! initial length
+      t = 0.                    ! initial time
+      do while(t<1.0)
+c     Loading condition 1
+         if (t.le.0.25) then
+            vel = 0.01
+         elseif (t.gt.0.25.and.t.le.0.55) then
+            vel =-0.01
+         else
+            vel = 0.01
+         endif
+         dl = vel * dt
+         deps = dl / l
+c        initially assuming all strain is elastic
+         deps_pl = 0.0
+         deps_el = deps
+c        guess on stress increment
+         dsig = E * deps_el
+         kount = 0
+         f = calc_yield_function( stress+ dsig) - c
+         iplast=0
+         do while (f.gt.tol.and.kount.lt.3) ! if exceeding plastic onset
+            iplast=1
+            deps_el = deps - deps_pl
+            dsig = e * deps_el
+            f = calc_yield_function(stress+dsig) - c
+c           estimate new plastic increment
+            deps_pl = deps_pl - f/(-E)*sign(1.,deps)
+            kount = kount +1
+         enddo
+         write(3,'(2f9.4,f10.6,f10.2,2i2)')t,l,eps,stress,iplast,kount
+         stress = stress + dsig
+         eps = eps + deps
+         t = t + dt
+         l = l + dl
+      enddo
+      close(3)
+      end program
+
+
+
 
 c     scalar yield function df(s)/ds = 1.
       real function calc_df()
@@ -32,61 +92,3 @@ c     calculate deps_pl_bar
       return
       end function
 c     ----------------------------
-      program elasto_plasticity_scalar
-      implicit none
-      real calc_yield_function
-      real calc_df
-      real dt, E, c, t, l, dl, eps, deps, deps_el, deps_pl,dsig,
-     $     stress
-      real vel, f, tol
-      integer kount
-      parameter(tol=1e-6)
-
-      open(3,file='elasto_plasticity_scalar.txt')
-
-      dt = 0.01                 ! time increment
-      E = 200000                ! elastic modulus
-      c = 200.                  ! yield criterion
-
-
-      stress = 0.               ! initial stress
-      eps = 0.                  ! initial strain
-
-      l = 1.                    ! initial length
-      t = 0.
-      vel = 0.01
-
-      dl = vel * dt
-
-      do while(t<0.5)
-         deps = dl / l
-c        initially assuming all strain is elastic
-         deps_pl = 0.0
-         deps_el = deps
-c        guess on stress increment
-         dsig = E * deps_el
-         kount = 0
-         f = calc_yield_function( stress+ dsig) - c
-c         write(*,*)'f:',f
-c         write(*,*)'stress+dsig:',stress+dsig
-         do while (f.gt.tol.and.kount.lt.3) ! if exceeding plastic onset
-c            write(*,*)'entering plasticity?'
-            deps_el = deps - deps_pl
-            dsig = e * deps_el
-            f = calc_yield_function(stress+dsig) - c
-c           estimate new plastic increment
-            deps_pl = deps_pl - f/(-E)
-c            write(*,*)'deps_pl',deps_pl
-            kount = kount +1
-         enddo
-         write(3,*)t,l,kount,eps,stress
-         stress = stress + dsig
-         eps = eps + deps
-         t = t + dt
-         l = l + dl
-      enddo
-
-
-      close(3)
-
-      end program
